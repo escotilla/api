e3cb3abfee2142562c5bf294d588e3edc7a584b1
Sam Stanton-Reid <stantonr@bu.edu>
2017-12-06 14:04:44 -0800

    Create Escotilla controller

---
+++ app/Http/Controllers/EscotillaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class EscotillaController extends Controller
+{
+    protected function successResponse($data)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ]);
+    }
+
+    protected function errorResponse($message, $code)
+    {
+        return response()
+            ->json([
+                'success' => false,
+                'message' => $message,
+                'code' => $code
+            ])
+            ->setStatusCode($code);
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
