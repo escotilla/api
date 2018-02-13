adf35464ae27fc1ad1758c622d82564a02871d51
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-12 22:15:21 -0800

    Add new question definitions

--- app/Question.php
+++ app/Question.php
@@ -18,12 +18,16 @@ use Jenssegers\Mongodb\Relations\HasMany;
 class Question extends Eloquent
 {
     const LOAN_AMOUNT = '5a54031fbfaf5700a0017672';
+    const INFO_ACCURATE = '5a7e03f9921ef3001409e164';
+    const AGREE_LOAN_CONTRACT = '5a7e670b921ef3001c00ace2';
 
     const BUSINESS_DESCRIPTION = '5a501d0ebfaf57001c265b22';
     const BUSINESS_PRODUCT = '5a5287a5bfaf57008c058792';
     const BUSINESS_NAME = '5a5287a5bfaf57008c058793';
 
     const DOB = '5a501d0ebfaf57001c265b23';
+    const FULL_NAME = '5a7e03f9921ef3001409e162';
+    const EMAIL = '5a7e03f9921ef3001409e163';
 
     /**
      * The attributes that are mass assignable.
