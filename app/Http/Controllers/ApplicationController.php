da94cc6b5d9234614c8ed198d1065ab3a0b5a6c0
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-12 22:13:30 -0800

    Update application endpoint

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -23,10 +23,6 @@ class ApplicationController extends EscotillaController
 
         $user = Auth::user();
 
-        if (is_null($user)) {
-            return $this->errorResponse('User not found', 404);
-        }
-
         $payload = $request->get('payload');
 
         $application = ApplicationFactory::fromPayload($user, $payload);
@@ -35,7 +31,63 @@ class ApplicationController extends EscotillaController
 
         $user->save();
 
-        return $this->successResponse($user->to_application_output());
+        return $this->successResponse($user->to_auth_output());
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'api_token' => 'required',
+            'payload' => 'required',
+            'application_id' => 'required'
+        ]);
+
+        $user = Auth::user();
+        $application = Application::find($request->get('application_id'));
+        $payload = $request->get('payload');
+
+        if (is_null($application)) {
+            return $this->errorResponse('Application not found', Response::HTTP_NOT_FOUND);
+        }
+
+        foreach ($payload as $question_id => $value) {
+            if ($question_id === Question::INFO_ACCURATE) {
+                $application->updateChecklist('review_profile', 'complete');
+            }
+
+            if ($question_id === Question::AGREE_LOAN_CONTRACT) {
+                $application->updateChecklist('sign_agreement', 'complete');
+            }
+
+            $savedAnswer = $application
+                ->answers()
+                ->first(function($answer) use ($question_id) {
+                return $answer->question->id == $question_id;
+            });
+
+            if (isset($savedAnswer)) {
+                if ($savedAnswer->answer === $value) {
+                    continue;
+                }
+
+                $application->answers()->destroy($savedAnswer);
+            }
+
+            $newAnswer = new Answer(['answer' => $value]);
+            $question = Question::find($question_id);
+
+            if (isset($question)) {
+                $newAnswer->question()->associate($question);
+                $newAnswer->application()->associate($application);
+                $newAnswer->save();
+
+                $application->answers()->associate($newAnswer);
+            }
+        }
+
+        $application->save();
+
+        return $this->successResponse($user->to_auth_output());
     }
 
     public function get(Request $request) {
@@ -49,7 +101,7 @@ class ApplicationController extends EscotillaController
             $this->errorResponse('User not found', 404);
         }
 
-        return $this->successResponse($user->to_application_output());
+        return $this->successResponse($user->to_auth_output());
     }
     /**
      * @param $user
@@ -72,7 +124,6 @@ class ApplicationController extends EscotillaController
 
             }
         }
-
 
         $user->business()->associate($business);
     }
