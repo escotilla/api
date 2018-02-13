1ea1ba81533143945f658c7528912495a6d48a66
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-12 22:13:46 -0800

    Update application factory

--- app/Factories/ApplicationFactory.php
+++ app/Factories/ApplicationFactory.php
@@ -14,27 +14,43 @@ class ApplicationFactory
     public static function fromPayload(User $user, $payload)
     {
         $application = new Application(['status' => 'pending_completion']);
-        $application = ChecklistFactory::createChecklist($application, ['review_profile', 'upload_documents', 'sign_agreement']);
+        $application = ChecklistFactory::createChecklist($application, [
+            'review_profile',
+            'upload_documents',
+            'sign_agreement'
+        ]);
         $application->save();
 
         foreach ($payload as $question_id => $value) {
-            $answer = new Answer(['answer' => $value]);
-            $question = Question::find($question_id);
-
-            if (isset($question)) {
-                $answer->question()->associate($question);
-                $answer->application()->associate($application);
-                $answer->save();
-
-                $application->answers()->associate($answer);
-                $application->save();
-
-                $user->applications()->associate($application);
-                $user->save();
-            }
+            self::saveAnswerToApplication($value, $question_id, $application);
         }
+
+        self::saveAnswerToApplication($user->name, Question::FULL_NAME, $application);
+        self::saveAnswerToApplication($user->email, Question::EMAIL, $application);
+
+        $application->save();
+        $user->applications()->save($application);
+        $user->save();
 
         return $application;
     }
 
+    /**
+     * @param $value
+     * @param $question_id
+     * @param $application
+     */
+    public static function saveAnswerToApplication($value, $question_id, $application)
+    {
+        $answer = new Answer(['answer' => $value]);
+        $question = Question::find($question_id);
+
+        if (isset($question)) {
+            $answer->question()->associate($question);
+            $answer->application()->associate($application);
+            $answer->save();
+
+            $application->answers()->associate($answer);
+        }
+    }
 }
